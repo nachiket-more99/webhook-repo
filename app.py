263c2d3d4d68bf0bd1bc5ab339e5c494206313f6
Nachiiet More <nachiketmore.more@gmail.com>
2026-01-29 19:10:27 +0530

    added PR event webhook

--- app.py
+++ app.py
@@ -9,8 +9,40 @@ def health():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    print("Received webhook!")
-    print(request.json)
+    event = request.headers.get("X-GitHub-Event")
+    data = request.json  
+    
+    print("Event:", event)   
+
+    # PUSH EVENT
+    if event == "push":
+        change_obj = {
+            "request_id": data["head_commit"]["id"],
+            "author": data["pusher"]["name"],
+            "action": "PUSH",
+            "from_branch": None,
+            "to_branch": data["ref"].split("/")[-1],
+            "timestamp": data["head_commit"]["timestamp"]
+        }
+        print(change_obj)
+
+    # PULL REQUEST EVENT
+    elif event == "pull_request":
+        pr = data["pull_request"]
+
+        change_obj = {
+            "request_id": pr["id"],
+            "author": pr["user"]["login"],
+            "action": "PULL_REQUEST",
+            "from_branch": pr["head"]["ref"],
+            "to_branch": pr["base"]["ref"],
+            "timestamp": pr["created_at"]
+        }
+        print(change_obj)
+
+    else:
+        print("Event:", event)
+
     return "", 200
 
 
