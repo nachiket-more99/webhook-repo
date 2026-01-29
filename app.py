4a8d57e6327c8fa561c093e7349e5f8aca8aae5a
Nachiiet More <nachiketmore.more@gmail.com>
2026-01-29 13:15:50 +0530

    added test push webhook

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
 @app.route("/")
 def health():
     return "OK"
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    print("Received webhook!")
+    print(request.json)
+    return "", 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
